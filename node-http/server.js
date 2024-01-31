ea00234361c20de9e970da6568c2987bbfe3b7b4
myminseok <myminseok@gmail.com>
2024-01-31 17:42:18 +0900

    fix app

--- node-http/server.js
+++ node-http/server.js
@@ -5,6 +5,7 @@ var port = process.env.PORT;
 http.createServer(function handler(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Workd\n');
-}).listen(port, ip.address());
+}).listen(port, "127.0.0.1");
 
 console.log('Server running at http://' + ip.address() +':'+port+'/');
+console.log('Server running at http://' + "127.0.0.1" +':'+port+'/');
