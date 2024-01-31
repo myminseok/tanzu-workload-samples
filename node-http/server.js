84cf6f4157930a85cf573dbce21a361a81696825
Minseok Kim <myminseok@users.noreply.github.com>
2024-01-31 18:20:21 +0900

    Update server.js

--- node-http/server.js
+++ node-http/server.js
@@ -6,4 +6,5 @@ http.createServer(function handler(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Workd\n');
 }).listen(port, "127.0.0.1");
+
 console.log('Server running at http://' + "127.0.0.1" +':'+port+'/');
