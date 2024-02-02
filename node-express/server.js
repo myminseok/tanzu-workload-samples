1e0f81dbb0cd282d30a167be4a82416b44ad6f48
Minseok Kim <myminseok@users.noreply.github.com>
2024-02-02 13:58:19 +0900

    Update server.js

--- node-express/server.js
+++ node-express/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server! v1');
+  res.send('Hello from the server! v0');
 });
 
 app.get('/health', (req, res) => {
