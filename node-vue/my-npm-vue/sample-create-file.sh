82829e1a90f088fcf9f8abb2d5f72c65e9804a5b
myminseok <myminseok@gmail.com>
2024-02-21 19:18:27 +0900

    update

--- node-vue/my-npm-vue/sample-create-file.sh
+++ node-vue/my-npm-vue/sample-create-file.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DEVELOPER_NAMESPACE=${1:-my-space}
+tanzu apps workload delete my-npm-vue -n my-space
 tanzu apps workload apply --file ./config/workload.yaml \
 	-n ${DEVELOPER_NAMESPACE} \
 	--debug --yes
